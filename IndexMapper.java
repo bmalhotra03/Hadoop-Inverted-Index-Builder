0fef936a50c30a48074f2bd3bbff4c1f1dbf273c
Brij <127152242+bmalhotra03@users.noreply.github.com>
2024-10-15 18:22:16 -0700

    Add files via upload

---
+++ IndexMapper.java
@@ -0,0 +1,21 @@
+package stubs;
+import java.io.IOException;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.apache.hadoop.fs.Path;
+
+public class IndexMapper extends Mapper<Text, Text, Text, Text> {
+  @Override
+  public void map(Text key, Text value, Context context) throws IOException,
+      InterruptedException {
+	  FileSplit fileSplit = (FileSplit)context.getInputSplit();
+	  Path path = fileSplit.getPath();
+	  String wordPlace = path.getName() + "@" + key.toString();
+	  Text location = new Text(wordPlace);
+	  String lc_line = value.toString().toLowerCase();
+	  for (String word: lc_line.split("\\W+")) {
+		  if (word.length() > 0) {
+			  context.write(new Text(word), location);
+		  }}}}  
